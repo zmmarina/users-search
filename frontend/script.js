ba46acbe7c430686fad4474da7442c258045127a
ZMarina Monteiro <zmarina.hm@gmail.com>
2020-07-25 17:19:50 -0300

    logic implemented

--- frontend/script.js
+++ frontend/script.js
@@ -1,24 +1,30 @@
 let globalUsers = [];
+let countUsers = 0;
+let countMales = 0;
+let countFemales = 0;
+let sumAges = 0;
+let averageAge = 0;
 
 async function start(){
     await fetchUsers();
 
     hideSpinner();
+    render();
 }
 
 async function fetchUsers(){
     const response = await fetch("http://localhost:3001/users");
     const json = await response.json();
-
-    globalUsers = json.map(({name, picture, login, age, gender}) =>{
-        return{
-            userId: login.uuid,
-            userName: name,
-            userAge: age,
-            userGender: gender,
-            userPicture: picture.large
-        }
-    })
+    globalUsers = json.map (user =>{
+        
+        return {
+            id: user.login.uuid,
+            name: user.name,
+            picture: user.picture.large,
+            age: user.dob.age,
+            gender: user.gender
+        };
+    });     
 }
 
 function hideSpinner(){
@@ -26,6 +32,64 @@ function hideSpinner(){
     spinner.classList.add('hide');
 }
 
-function render(){}
+function render(){
+    renderUsers();
+    renderTitle();  
+    renderStatistics();
+};
+
+function renderUsers(){
+    let usersHTML = '<div>';
+
+    globalUsers.forEach(user=>{
+        const { id, name, picture, age, gender} = user;
+ 
+        const userHTML = `
+        <div class='user'>
+         <div>
+             <img src="${picture}" alt="${name}">
+         </div>
+         <div>
+             ${name.first} ${name.last}, ${age} years
+         </div>         
+        </div>       
+        `;
+        usersHTML+= userHTML;
+    });
+ 
+    const divUsers = document.querySelector("#users");
+     divUsers.innerHTML = usersHTML;   
+}
+
+function renderTitle(){
+    const countUsers = document.querySelector("#countUsers");
+    countUsers.textContent = globalUsers.length;
+}
+
+function renderStatistics(){
+    const countMales = document.querySelector("#countMales");
+    const countFemales = document.querySelector("#countFemales");
+    const mappedGenders = globalUsers.map(user=> user.gender);
+    const sumAges = document.querySelector("#sumAges");
+    const averageAge = document.querySelector("#averageAge");
+
+
+    let male = mappedGenders.filter(gender => {
+        return gender === "male"});
+    countMales.textContent = male.length;
+
+    let female = mappedGenders.filter(gender => {
+        return gender === "female"});
+    countFemales.textContent = female.length;
+
+    const totalAges = globalUsers.reduce((acc,curr)=>{
+        return acc + curr.age;
+    },0);
+    sumAges.textContent = totalAges;
+
+    averageAge.textContent = totalAges/globalUsers.length;
+
+          
+}
 
 start();
